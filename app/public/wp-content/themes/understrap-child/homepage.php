94f4b4253c9fc40c7004478681feb56cadead929
Robyn <127926809+Robyn-web@users.noreply.github.com>
2025-02-01 17:18:39 +0000

    Added basic empty page layout to homepage.php template

--- app/public/wp-content/themes/understrap-child/homepage.php
+++ app/public/wp-content/themes/understrap-child/homepage.php
@@ -1,5 +1,17 @@
 <?php
 /*
-Template Name: Custom Page Template
+Template Name: homepage template
+@package Understrap
 */
-?>
+
+// Exit if accessed directly.
+defined( 'ABSPATH' ) || exit;
+
+get_header();
+
+while ( have_posts() ) :
+	the_post();
+	get_template_part( 'loop-templates/content', 'empty' );
+endwhile;
+
+get_footer();
--- app/public/wp-content/themes/understrap/header.php
+++ app/public/wp-content/themes/understrap/header.php
@@ -28,7 +28,7 @@ $navbar_type       = get_theme_mod( 'understrap_navbar_type', 'collapse' );
 
 	<!-- ******************* The Navbar Area ******************* -->
 	<header id="wrapper-navbar">
-
+        
 		<a class="skip-link <?php echo understrap_get_screen_reader_class( true ); ?>" href="#content">
 			<?php esc_html_e( 'Skip to content', 'understrap' ); ?>
 		</a>
